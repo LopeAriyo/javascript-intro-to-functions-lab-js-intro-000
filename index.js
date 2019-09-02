cfd18c14d3d2bcb0625133fc48ba53b97cf47021
Lopè Ariyo <lopeariyo@gmail.com>
2019-09-02 09:16:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("Hello".toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if string === lowercase {
+    return "I can't hear you!"
+  }else if string === uppercase {
+    return "YES INDEED!"
+  } else if string === "I love you, Grandma."{
+  return "I love you, too."
+  }
+
+}
