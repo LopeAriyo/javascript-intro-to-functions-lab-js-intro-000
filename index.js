f746f3c83c3237cfa41aa7c75ce0d4527b711e70
Lopè Ariyo <lopeariyo@gmail.com>
2019-09-02 09:12:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log("hello".toUpperCase)
 }
 function logWhisper(string) {
-  console.log(string.toUpperCase)
+  console.log("Hello".toUpperCase)
 }
